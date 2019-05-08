fe0bcecdbbfa79e9e076fbe88d38171c1545700f
xingxiping <xiping.xing@finupgroup.com>
2019-05-09 01:26:29 +0800

    我的。。页面

---
+++ src/main/java/com/order_food/web/dao/NoticeInfoDao.java
@@ -0,0 +1,24 @@
+package com.order_food.web.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.order_food.web.entity.model.NoticeInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * 功能描述:dao层  ，数据库持久层类，(表USER)
+ *
+ * @author yln
+ * @version 1.0.0
+ */
+@Mapper
+@Component
+public interface NoticeInfoDao extends BaseMapper<NoticeInfo> {
+
+}
+
+
+
+
+
